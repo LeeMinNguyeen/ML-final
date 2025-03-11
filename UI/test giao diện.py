55a6da65827b6afc1184b195eadc1161d554a7e6
kiuu1524 <nkieu1502@gmail.com>
2025-03-11 11:00:39 +0700

    ML

--- UI/test giao diện.py
+++ UI/test giao diện.py
@@ -1,8 +1,10 @@
+from UI.LoginWindow import Ui_LoginWindow
+
 if __name__ == "__main__":
     import sys, QtWidgets
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_Form()
+    ui = Ui_LoginWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec())
